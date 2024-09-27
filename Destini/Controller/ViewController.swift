d96e1289b1e8261e807dac9abd1923a01c63e4fa
murad <muradkv@gmail.com>
2024-09-27 21:20:00 +0300

    Initial commit

---
+++ Destini/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by murad on 27.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
